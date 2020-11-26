f313b0d9e567710b776012fb6894bef3dd4f2110
20iyerd <deetya.iyer@gmail.com>
2020-11-26 00:28:22 -0500

    separate class for last.fm profile

---
+++ lastfm.py
@@ -0,0 +1,31 @@
+import requests
+
+class LastFMUser(object):
+    api_key = '43a1115a43350f02986daf50d4e99a9a'
+    url = 'http://ws.audioscrobbler.com/2.0'
+    def __init__(self,username):
+        self.username = username
+        self.headers = {
+            'user-agent': self.username
+        }
+
+    def setUsername(self,username):
+        self.username = username
+    
+    def lastFMGet(self,payload):
+        payload['api_key'] = LastFMUser.api_key
+        payload['format'] = 'xml'
+        response = requests.get(LastFMUser.url,headers=self.headers,params=payload)
+        return response.text
+
+    def getAlbumInfo(self,artist,album):
+        return self.lastFMGet({'method':'album.getInfo','artist':artist,'album':album})
+
+    def getArtistTopAlbums(self,artist):
+        return self.lastFMGet({'method':'artist.getTopAlbums','artist':artist})
+
+    def getUserInfo(self):
+        return self.lastFMGet({'method':'user.getInfo','user':self.username})
+    
+    def getUserLovedTracks(self):
+        return self.lastFMGet({'method':'user.getLovedTracks','user':self.username})
