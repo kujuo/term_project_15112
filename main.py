65ae7cd204420153e90bfbf91d983339f2b07c6b
20iyerd <deetya.iyer@gmail.com>
2020-11-26 00:29:18 -0500

    deleted tech demo comments

--- main.py
+++ main.py
@@ -1,92 +1,11 @@
-# Deetya Iyer
-# file for tech demo: 2020-11-23
-# modules to demo:
-    # pygame mixer
-    # last.fm api
-    # HTTP requests
-# other modules (included w python, not external modules):
-    # date/time
-    # ElementTree
-# resources referenced:
-    # last.fm API:
-        # https://www.dataquest.io/blog/last-fm-api-python/
-        # https://www.last.fm/api/
-    # pygame mixer:
-        # https://www.geeksforgeeks.org/python-playing-audio-file-in-pygame/
-    # XML file IO (not being demo-ed):
-        # https://www.geeksforgeeks.org/reading-and-writing-xml-files-in-python/
-    # CMU 112 graphics:
-        # course notes (https://www.cs.cmu.edu/~112/notes/notes-animations-part3.html)
-    # HTTP requests:
-        # my old code from high school (AP research class)
-        # same resource as last.fm API
-
-
 from cmu_112_graphics import *
+from playlist import *
+from lastfm import *
 from pygame import mixer
-import requests
 import os
-from dataclasses import make_dataclass
 
-Song = make_dataclass('Song',['title','path'])
-
-class Playlist(object):
-    numPlaylists = 0
-    def __init__(self,title,parent):
-        self.title = title
-        self.parent = parent
-        self.songs = []
-
-    def addSong(self,song):
-        self.songs.append(song)
-    
-    def incrementPlaylists():
-        Playlist.numPlaylists += 1
-
-    def getSongs(self):
-        if self.parent != None:
-            return self.parent.getSongs() + self.songs
-        else:
-            return self.songs
-    
-    def addParent(self,parent):
-        self.parent = parent
-
-class LastFMUser(object):
-    api_key = '43a1115a43350f02986daf50d4e99a9a'
-    url = 'http://ws.audioscrobbler.com/2.0'
-    def __init__(self,username):
-        self.username = username
-        self.headers = {
-            'user-agent': self.username
-        }
-
-    def setUsername(self,username):
-        self.username = username
-    
-    def lastFMGet(self,payload):
-        payload['api_key'] = LastFMUser.api_key
-        payload['format'] = 'xml'
-        response = requests.get(LastFMUser.url,headers=self.headers,params=payload)
-        return response.text
-
-    def getAlbumInfo(self,artist,album):
-        return self.lastFMGet({'method':'album.getInfo','artist':artist,'album':album})
-
-    def getArtistTopAlbums(self,artist):
-        return self.lastFMGet({'method':'artist.getTopAlbums','artist':artist})
-
-    def getUserInfo(self):
-        return self.lastFMGet({'method':'user.getInfo','user':self.username})
-    
-    def getUserLovedTracks(self):
-        return self.lastFMGet({'method':'user.getLovedTracks','user':self.username})
-
-    
-
-class MyApp(App):
+class MainScreen(App):
     def appStarted(self):
-
         # pygame mixer stuff
         mixer.init()
         self.rootDir = '/home/dee/code/schoolwork/15112/term_project/trial_runs/music_examples/'
@@ -143,6 +62,8 @@ class MyApp(App):
         elif event.key == 'q':
             numSongs = self.loadQueue()
             print(f'queueing {numSongs} songs.')
+        elif event.key == 't':
+            pass
         elif event.key == 'Left':
             self.skipCount -= 1
             mixer.music.unload()
@@ -161,6 +82,7 @@ class MyApp(App):
     def playlistClicked(self,x,y):
         return (self.buttons['playlist'][0] <= x <= self.buttons['playlist'][2] and
                 self.buttons['playlist'][1] <= y <= self.buttons['playlist'][3])
+
     
     def importClicked(self,x,y):
         return (self.buttons['dataImport'][0] <= x <= self.buttons['dataImport'][2] and
@@ -201,4 +123,4 @@ class MyApp(App):
     def redrawAll(self,canvas):
         self.drawButtons(canvas)
 
-MyApp(width=600, height=600)
+MainScreen(width=600, height=600)
