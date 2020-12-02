94db1ea29a0811d3ad906922825a2074b9d14d20
20iyerd <deetya.iyer@gmail.com>
2020-12-02 02:51:54 -0500

    added data mode

--- welcome.py
+++ welcome.py
@@ -12,6 +12,7 @@ class WelcomeMode(Mode):
         canvas.create_text(mode.width//2,mode.height//2,text='press enter to... you know... enter',fill=scheme.getAccent1(),font=fonts['accent'])
         canvas.create_text(mode.width//2,mode.height//1.8,text='settings: s',fill=scheme.getAccent1(),font=fonts['accent'])
         canvas.create_text(mode.width//2,mode.height//1.65,text='help: h',fill=scheme.getAccent1(),font=fonts['accent'])
+        canvas.create_text(mode.width//2,mode.height//1.6,text='my stats: d',fill=scheme.getAccent1(),font=fonts['accent'])
         canvas.create_text(mode.width//2,mode.height//1.5,text='check in: space',fill=scheme.getAccent1(),font=fonts['accent'])
     
     def keyPressed(mode,event):
@@ -21,5 +22,7 @@ class WelcomeMode(Mode):
             mode.app.setActiveMode(mode.app.settingsMode)
         elif event.key == 'h':
             mode.app.setActiveMode(mode.app.helpMode)
+        elif event.key == 'd':
+            mode.app.setActiveMode(mode.app.dataMode)
         elif event.key == 'Space':
             mode.app.setActiveMode(mode.app.checkInMode)
