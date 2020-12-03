b41a3c6a61f00ee6526dc7b5012df634629b69a9
20iyerd <deetya.iyer@gmail.com>
2020-12-03 10:32:40 -0500

    switched parameter order, added album cover URL

--- lastfm.py
+++ lastfm.py
@@ -1,9 +1,13 @@
 import requests
 from apikey import *
+import xml.etree.ElementTree as ET
 
 # referenced these websites for help on syntax/methods
 # https://www.dataquest.io/blog/last-fm-api-python/
 # https://www.last.fm/api/
+
+# all nusic-related images (album covers, artist thumbnails)
+# from last.fm database
 
 class LastFMUser(object):
     url = 'http://ws.audioscrobbler.com/2.0'
@@ -23,8 +27,18 @@ class LastFMUser(object):
         response = requests.get(LastFMUser.url,headers=self.headers,params=payload)
         return response.text
 
-    def getAlbumInfo(self,artist,album):
+    def getAlbumInfo(self,album,artist):
         return self.lastFMGet({'method':'album.getInfo','artist':artist,'album':album})
+
+    # def getAlbumSearchInfo(self,album,artist)
+    
+    def getAlbumCoverURL(self,album,artist):
+        albumInfo = str(self.getAlbumInfo(album,artist))
+        tree = ET.fromstring(albumInfo)
+        if tree.find('./album/image[@size="large"]') != None:
+            return tree.find('./album/image[@size="large"]').text
+        else:
+            return 'default.png'
 
     def getArtistTopAlbums(self,artist):
         return self.lastFMGet({'method':'artist.getTopAlbums','artist':artist})
