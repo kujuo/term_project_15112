e943d878c10484ef2aafc607ae9d1981f64df80c
20iyerd <deetya.iyer@gmail.com>
2020-12-03 10:25:39 -0500

    added album data visualization

--- data.py
+++ data.py
@@ -1,10 +1,15 @@
 from cmu_112_graphics import *
+from lastfm import *
+from design import *
+from xml_io import *
 
 class DataMode(Mode):
     def appStarted(mode):
         # aaaaaaaaaaaa
         # use mode. instead of mode.
-        pass
+        mode.topSongs = songsXML.getRankedSongs().getSongs()
+        mode.images = []
+        mode.loadImages()
 
     def timerFired(mode):
         pass
@@ -13,7 +18,22 @@ class DataMode(Mode):
         pass
 
     def keyPressed(mode,event):
-        pass
+        if event.key == 'y':
+            print(songsXML.getRankedSongs().getSongs())
+
+    def loadImages(mode):
+        for song in mode.topSongs:
+            url = user.getAlbumCoverURL(song.album,song.artist)
+            image = mode.loadImage(url)
+            scale = (songsXML.getPlayCount(song))/(songsXML.getPlayCount(mode.topSongs[0]))
+            mode.images.append(mode.scaleImage(image,scale))
+
+    def drawTopSongs(mode,canvas):
+        yPos = 50
+        for image in mode.images:
+            canvas.create_image(50,yPos,image=ImageTk.PhotoImage(image))
+            yPos += 100
 
     def redrawAll(mode,canvas):
         canvas.create_rectangle(0,0,mode.width,mode.height,fill=scheme.getFill())
+        mode.drawTopSongs(canvas)
