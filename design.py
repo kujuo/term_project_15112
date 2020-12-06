991c34ff3774d6674e74927e271c725874e31af1
20iyerd <deetya.iyer@gmail.com>
2020-12-05 22:23:51 -0500

    deleted unnecessary comment

--- design.py
+++ design.py
@@ -28,8 +28,6 @@ class ColorScheme(object):
     # copied from graphics course notes
     # https://www.cs.cmu.edu/~112/notes/notes-graphics.html
     def rgbString(self, r, g, b):
-        # Don't worry about the :02x part, but for the curious,
-        # it says to use hex (base 16) with two digits.
         return f'#{r:02x}{g:02x}{b:02x}'
     
     def setColor(self,color):
