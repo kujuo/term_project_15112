0b1b5613f2f12688b54f6d5d15b980fd61174234
20iyerd <deetya.iyer@gmail.com>
2020-12-08 23:11:19 -0500

    cleaned up data, deleted dead code

--- data.py
+++ data.py
@@ -14,8 +14,6 @@ class DataMode(Mode):
         mode.viewModes = {
             'top5':['','track','album','artist'],
             'today':['','playback'],
-            'week':['playback','listening'],
-            'month':['playback','listening'],
             'all time':['','playback'],
             'faves':['','covers','titles'],
             'onehits':['','covers','titles']
@@ -25,10 +23,6 @@ class DataMode(Mode):
                    mode.width//1.5+5,60),
             'today':(mode.width//1.5-5,70,
                       mode.width//1.5+5,80),
-            'week':(mode.width//1.5-5,90,
-                     mode.width//1.5+5,100),
-            'month':(mode.width//1.5-5,110,
-                        mode.width//1.5+5,120),
             'all time':(mode.width//1.5-5,130,
                      mode.width//1.5+5,140),
             'faves':(mode.width//1.5-5,150,
@@ -53,14 +47,10 @@ class DataMode(Mode):
             elif key == '2':
                 mode.currentMode = 'today'
             elif key == '3':
-                mode.currentMode = 'week'
-            elif key == '4':
-                mode.currentMode = 'month'
-            elif key == '5':
                 mode.currentMode = 'all time'
-            elif key == '6':
+            elif key == '4':
                 mode.currentMode = 'faves'
-            elif key == '7':
+            elif key == '5':
                 mode.currentMode = 'onehits'
     
     def handleLRKey(mode,key):
@@ -119,7 +109,7 @@ class DataMode(Mode):
                     mode.loadOneHits()
 
     def keyPressed(mode,event):
-        elif event.key == 'x':
+        if event.key == 'x':
             if mode.homeScreen:
                 mode.app.setActiveMode(mode.app.welcomeMode)
             else:
@@ -145,23 +135,12 @@ class DataMode(Mode):
         mode.totalListeningTime = userXML.getTotalListeningTime()//60
         mode.totalListeningDays = userXML.getTotalListeningDays()
 
-    # def loadWeekTopSongImages(mode):
-    #     mode.images = []
-    #     topSongs = userXML.getWeekTopSongs(datetime.date.today())[:5]
-    #     for song in topSongs:
-    #         url = user.getAlbumCoverURL(song['album'],song['artist'])
-    #         image = mode.loadImage(url)
-    #         scale = 0.5
-    #         mode.images.append((mode.scaleImage(image,scale),song['title'],song['artist'],song['playcount']))
-            # mode.totalListeningTime = int(userXML.getWeekListeningTime(datetime.date.today()))//60
-
     def loadDayTopSong(mode):
         mode.images = []
         topSongs = userXML.getDayTopSongs(datetime.date.today())[:5]
         for song in topSongs:
             url = user.getAlbumCoverURL(song['album'],song['artist'])
             image = mode.loadImage(url)
-            # scale = (songsXML.getPlayCount(song))/(songsXML.getPlayCount(topSongs[0]))
             scale = 0.5
             mode.images.append((mode.scaleImage(image,scale),song['title'],song['artist'],song['playcount']))
             mode.totalListeningTime = int(userXML.getDayListeningTime(datetime.date.today()))//60
@@ -173,7 +152,6 @@ class DataMode(Mode):
         for song in topSongs:
             url = user.getAlbumCoverURL(song.album,song.artist)
             image = mode.loadImage(url)
-            # scale = (songsXML.getPlayCount(song))/(songsXML.getPlayCount(topSongs[0]))
             scale = 1
             mode.images.append((mode.scaleImage(image,scale),song.title,song.artist,song.playcount))
     
@@ -184,7 +162,6 @@ class DataMode(Mode):
         for element in topAlbums:
             url = user.getAlbumCoverURL(element['album'],element['artist'])
             image = mode.loadImage(url)
-            # scale = int(element['playcount'])/topAlbumPlaycount
             scale = 1
             mode.images.append((mode.scaleImage(image,scale),element['album'],element['artist'],element['playcount']))
     
@@ -205,8 +182,7 @@ class DataMode(Mode):
         faves = userXML.getConsistentFaves()
         scale = 1
         for song in faves.getSongs()[:15]:
-            # url = user.getAlbumCoverURL(song.album,song.artist)
-            url = 'default.png'
+            url = user.getAlbumCoverURL(song.album,song.artist)
             image = mode.loadImage(url)
             print(song.title)
             mode.images.append((mode.scaleImage(image,scale),song.title,song.artist,song.playcount))
@@ -220,8 +196,7 @@ class DataMode(Mode):
         onehits = userXML.getOneHitWonders()
         scale = 1
         for song in onehits.getSongs()[:15]:
-            # url = user.getAlbumCoverURL(song.album,song.artist)
-            url = 'default.png'
+            url = user.getAlbumCoverURL(song.album,song.artist)
             image = mode.loadImage(url)
             print(song.title)
             mode.images.append((mode.scaleImage(image,scale),song.title,song.artist,song.playcount))
@@ -237,20 +212,14 @@ class DataMode(Mode):
         canvas.create_text(50,(mode.homeButtons['today'][1]+mode.homeButtons['today'][3])//2,
                                text="today's stats ( 2 )",font=fonts['accent'],
                                fill=scheme.getAccent1(),anchor='w')
-        canvas.create_text(50,(mode.homeButtons['week'][1]+mode.homeButtons['week'][3])//2,
-                               text="this week's stats ( 3 )",font=fonts['accent'],
-                               fill=scheme.getAccent1(),anchor='w')
-        canvas.create_text(50,(mode.homeButtons['month'][1]+mode.homeButtons['month'][3])//2,
-                               text="this month's stats ( 4 )",font=fonts['accent'],
-                               fill=scheme.getAccent1(),anchor='w')
         canvas.create_text(50,(mode.homeButtons['all time'][1]+mode.homeButtons['all time'][3])//2,
-                               text="all time stats ( 5 )",font=fonts['accent'],
+                               text="all time stats ( 3 )",font=fonts['accent'],
                                fill=scheme.getAccent1(),anchor='w')
         canvas.create_text(50,(mode.homeButtons['faves'][1]+mode.homeButtons['faves'][3])//2,
-                               text="favorites ( 6 )",font=fonts['accent'],
+                               text="favorites ( 4 )",font=fonts['accent'],
                                fill=scheme.getAccent1(),anchor='w')
         canvas.create_text(50,(mode.homeButtons['onehits'][1]+mode.homeButtons['onehits'][3])//2,
-                               text="your one-hit wonders ( 7 )",font=fonts['accent'],
+                               text="your one-hit wonders ( 5 )",font=fonts['accent'],
                                fill=scheme.getAccent1(),anchor='w')
 
     def drawDataPage(mode,canvas):
@@ -258,10 +227,6 @@ class DataMode(Mode):
             mode.drawTop5(canvas)
         elif mode.currentMode == 'today':
             mode.drawToday(canvas)
-        elif mode.currentMode == 'week':
-            pass
-        elif mode.currentMode == 'month':
-            pass
         elif mode.currentMode == 'all time':
             mode.drawAllTime(canvas)
         elif mode.currentMode == 'faves' or mode.currentMode == 'onehits':
@@ -350,9 +315,6 @@ class DataMode(Mode):
                 canvas.create_text(10,20+dHeight*1.5,text=element[1]+ ': '+element[3],fill=scheme.getAccent2(),font=fonts['accent'],anchor='w')
                 canvas.create_text(mode.width-10,20+dHeight*1.5,text=element[2],fill=scheme.getAccent2(),font=fonts['accent2'],anchor='e')
                 dHeight += 20
-
-    # def drawWeek(mode,canvas):
-    #     pass
 
     def drawAllTime(mode,canvas):
         view = mode.viewModes[mode.currentMode][mode.currentModePos]
