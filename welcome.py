79e422cc46de843b57e8f19327ad8032dbf569fc
20iyerd <deetya.iyer@gmail.com>
2020-12-08 23:11:02 -0500

    finally fixed the ugly welcome page lol

--- welcome.py
+++ welcome.py
@@ -10,10 +10,10 @@ class WelcomeMode(Mode):
         canvas.create_rectangle(0,0,mode.width,mode.height,fill=scheme.getFill())
         canvas.create_text(mode.width//2,mode.height//3,text='attune.',fill=scheme.getAccent1(),font=fonts['title'])
         canvas.create_text(mode.width//2,mode.height//2,text='press enter to... you know... enter',fill=scheme.getAccent1(),font=fonts['accent'])
-        canvas.create_text(mode.width//2,mode.height//1.8,text='settings: s',fill=scheme.getAccent1(),font=fonts['accent'])
-        canvas.create_text(mode.width//2,mode.height//1.65,text='help: h',fill=scheme.getAccent1(),font=fonts['accent'])
-        canvas.create_text(mode.width//2,mode.height//1.6,text='my stats: d',fill=scheme.getAccent1(),font=fonts['accent'])
-        canvas.create_text(mode.width//2,mode.height//1.5,text='check in: space',fill=scheme.getAccent1(),font=fonts['accent'])
+        canvas.create_text(mode.width//2,mode.height//2+30,text='settings: s',fill=scheme.getAccent1(),font=fonts['accent'])
+        canvas.create_text(mode.width//2,mode.height//2+50,text='help: h',fill=scheme.getAccent1(),font=fonts['accent'])
+        canvas.create_text(mode.width//2,mode.height//2+70,text='my stats: d',fill=scheme.getAccent1(),font=fonts['accent'])
+        canvas.create_text(mode.width//2,mode.height//2+90,text='check in: space',fill=scheme.getAccent1(),font=fonts['accent'])
     
     def keyPressed(mode,event):
         if event.key == 'Enter':
